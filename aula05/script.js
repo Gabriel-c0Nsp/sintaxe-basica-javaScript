936e148aa53383adde298ace11388b1488b6d14d
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-08-22 20:17:15 -0300

    aula sobre funções

---
+++ aula05/script.js
@@ -0,0 +1,13 @@
+// FUNÇÕES
+
+// function funcao() {
+//   console.log("Essa mensagem veio de uma função")
+// }
+
+// funcao();
+
+function mensagem(primeiro, segundo) {
+  console.log(primeiro, segundo)
+}
+
+mensagem("Tudo", "bem?")
