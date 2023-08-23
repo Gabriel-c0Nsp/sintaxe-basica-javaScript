a1387fd9ec83427c4a57e88f7161fe4ada547c3d
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-08-22 21:30:12 -0300

    Conteúdo da aula 6 com aprofundamento em funções

---
+++ aula06/script.js
@@ -0,0 +1,37 @@
+// APROFUNDANDO EM FUNÇÕES
+
+// tipos de funções
+
+// declarativa
+
+// function requerNome() {
+//   console.log('Sou uma mensagem de uma função declarativa')
+// }
+
+// requerNome()
+
+
+// expressão de função
+
+// com nomeação
+// var funcao = function funcao() {
+//   console.log('Sou uma mensagem de funcão de expressão!')
+// }
+
+// funcao()
+
+// sem nomeação
+
+// var funcao = function() {
+//   console.log('Sou uma mensagem de funcão de expressão!')
+// }
+
+// funcao()
+
+
+// arrow function
+// var funcao = () => {
+//   console.log('Sou uma arrow function!')
+// }
+
+// funcao()
